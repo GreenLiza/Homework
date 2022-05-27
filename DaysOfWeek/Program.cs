15f99b4d5051cb3d182c2972e483e2aee1106c91
User <User@MYHP>
2022-05-27 17:35:22 +0300

    Add project files.

---
+++ DaysOfWeek/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace DaysOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter day number (from 1 to 7) or 'exit' to quit");
+                string UserInput = Console.ReadLine();
+                if (UserInput == "exit")
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+                else if (Int32.TryParse(UserInput, out int x))
+                {
+                    FindDay(x);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input");
+                }
+            }
+        }
+
+        static void FindDay(int dayNumber)
+        {
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Out of range");
+                    break;
+            }
+        }
+    }
+}
---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userAnswer;
+            do
+            {
+                Console.WriteLine("Enter any number:");
+                if (Int32.TryParse(Console.ReadLine(), out int x))
+                {
+                    Multiplicate(x);
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input");
+                }
+                Console.WriteLine("If you want to repeat - press enter. If you want to quit - enter 'exit'.");
+                userAnswer = Console.ReadLine();
+            } while (userAnswer != "exit");
+            
+
+        }
+
+        static void Multiplicate(int number)
+        {
+            for (int i=1; i<11; i++)
+            {
+                Console.WriteLine($"{number}*{i}={number * i}");
+            }
+        }
+    }
+}
